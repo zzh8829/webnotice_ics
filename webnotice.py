59b9242a8491a1625b45fbbcf802cd2e4ed57c0e
Zihao Zhang <zzh8829@gmail.com>
2017-05-15 10:10:00 -0400

    Refresh

--- webnotice.py
+++ webnotice.py
@@ -19,50 +19,55 @@ def get_listing(dept):
     bs = BeautifulSoup(requests.get(wnotice+'/list_notices_p.pl?dept='+dept+'&time_frame=year').text, 'html5lib')
     events = []
     for dd in bs.find_all('dd'):
-        dt2 = dd.previous_sibling
-        dt1 = dt2.previous_sibling
-
-        when = dt1.find('b').text.strip()
-        local_tz = pytz.timezone ('America/Toronto')
-        parsed_dt = datetime.datetime.strptime(when, '%A, %d %B %Y, %I:%M%p')
-        local_dt = local_tz.localize(parsed_dt, is_dst=None)
-        utc_dt = local_dt.astimezone (pytz.utc)
-
         event = {}
-        event['when'] = utc_dt.strftime('%Y%m%dT%H%M00Z')
-        event['when_end'] = (utc_dt + datetime.timedelta(hours=1)).strftime('%Y%m%dT%H%M00Z')
-        event['seq'] = utc_dt.strftime('%Y%m%d%H')
+        try:
+            dt2 = dd.previous_sibling
+            dt1 = dt2.previous_sibling
 
-        event['venue'] = dt2.find('em').text.strip()
-        event['where'] = dt1.text.split('--')[1].replace('true', '').strip()
-        if '--' in dt2.text:
-            event['dept'] = dt2.text.split('--')[1].strip()
+            when = dt1.find('b').text.strip()
+            local_tz = pytz.timezone ('America/Toronto')
+            parsed_dt = datetime.datetime.strptime(when, '%A, %d %B %Y, %I:%M%p')
+            local_dt = local_tz.localize(parsed_dt, is_dst=None)
+            utc_dt = local_dt.astimezone (pytz.utc)
 
-        for em in dd.find_all('em'):
-            if em.text == 'Speaker:':
-                line = em.next_sibling.strip()
-                if 'Department' in line:
-                    event['who'] = line.rsplit(', Department')[0].strip()
-                    event['affiliation'] = 'Department ' + line.rsplit('Department')[1].strip()
-                else:
-                    event['who'] = line.rsplit(',')[0].strip()
-                    event['affiliation'] = line.rsplit(',')[1].strip()
-            elif em.text == 'Title:':
-                title = em.next_sibling.next_sibling.find('em').text.strip()
-                if title[:1] == '"':
-                    title = title[1:]
-                if title[-1:] == '"':
-                    title = title[:-1]
-                title = title.replace('$', '')
-                event['title'] = title
-            elif em.text == 'Abstract:':
-                # XXX TODO FIXME: This will cut off at the end of first paragraph
-                event['abstract'] = em.next_sibling.strip()
-            elif em.text == 'Remarks:':
-                event['remarks'] = em.next_sibling.strip()
+            event['when'] = utc_dt.strftime('%Y%m%dT%H%M00Z')
+            event['when_end'] = (utc_dt + datetime.timedelta(hours=1)).strftime('%Y%m%dT%H%M00Z')
+            event['seq'] = utc_dt.strftime('%Y%m%d%H')
 
-        event['uid'] = utc_dt.strftime('%Y')+'_'+hashlib.md5((event['who']+'|'+event['title']).encode('utf-8')).hexdigest()+'.'+emailfrom
-        events.append(event)
+            event['venue'] = dt2.find('em').text.strip()
+            event['where'] = dt1.text.split('--')[1].replace('true', '').strip()
+            if '--' in dt2.text:
+                event['dept'] = dt2.text.split('--')[1].strip()
+
+            for em in dd.find_all('em'):
+                if em.text == 'Speaker:':
+                    line = em.next_sibling.strip()
+                    if 'Department' in line:
+                        event['who'] = line.rsplit(', Department')[0].strip()
+                        event['affiliation'] = 'Department ' + line.rsplit('Department')[1].strip()
+                    else:
+                        event['who'] = line.rsplit(',')[0].strip()
+                        event['affiliation'] = line.rsplit(',')[1].strip()
+                elif em.text == 'Title:':
+                    title = em.next_sibling.next_sibling.find('em').text.strip()
+                    if title[:1] == '"':
+                        title = title[1:]
+                    if title[-1:] == '"':
+                        title = title[:-1]
+                    title = title.replace('$', '')
+                    event['title'] = title
+                elif em.text == 'Abstract:':
+                    # XXX TODO FIXME: This will cut off at the end of first paragraph
+                    event['abstract'] = em.next_sibling.strip()
+                elif em.text == 'Remarks:':
+                    event['remarks'] = em.next_sibling.strip()
+
+            event['uid'] = utc_dt.strftime('%Y')+'_'+hashlib.md5((event['who']+'|'+event['title']).encode('utf-8')).hexdigest()+'.'+emailfrom
+            events.append(event)
+
+            print(event['when'], event['who'])
+        except Exception as e:
+            print(dd, e)
     return events
 
 def dump_ics(dept, name):
@@ -94,6 +99,8 @@ def dump_ics(dept, name):
 
 if __name__ == '__main__':
     depts = get_depts()
-    print(depts)
+    # print(depts)
     for dept in depts:
+        print(depts[dept])
         dump_ics(dept, depts[dept])
+    #dump_ics('all_depts', depts['all_depts'])
